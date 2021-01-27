df31fd33dee55343497f795111cd732cf074f824
sangminpark <qpwpep5429@gmail.com>
2021-01-27 16:09:19 +0900

    ~1/27 JS

--- January/JavaScript/VanillaJS/maplestory/termRelated.js
+++ January/JavaScript/VanillaJS/maplestory/termRelated.js
@@ -8,7 +8,6 @@ termSubmit.addEventListener("click", removeSpecial);
 function removeSpecial(e) {
   e.target.value = e.target.value.replace(/[^ㄱ-힣a-zA-Z0-9+#]/gi, "");
 }
-const relatedTerm = termBox.value;
 
 termSubmit.addEventListener(
   "click",
@@ -34,10 +33,15 @@ function addRelatedTerm(name) {
   liElem.innerText = trimmedTerm;
 
   const removeBtn = document.createElement("button");
-  termList.appendChild(liElem);
-  liElem.appendChild(removeBtn);
-  removeBtn.innerHTML = "X";
-
+  const item = document.querySelectorAll("#list_terms > li");
+  for (let i = 0; i < item.length; i++) {
+    if (item[i].innerText === name) {
+      alert("good");
+    } else {
+      alert("중복입니다.");
+      termBox.value = "";
+    }
+  }
   termBox.value = "";
   removeBtn.addEventListener("click", () => {
     removeBtn.parentNode.removeChild(removeBtn);
@@ -45,9 +49,9 @@ function addRelatedTerm(name) {
   });
 }
 
-// click, keyup 둘 다 'termBox.value != ""' 를 기준으로 실행되게 하고 있는데,
-// 별로 좋은 방법이 아닌 듯 하다. 메인 addRelatedTerm 함수에서 termBox.value가 없을 경우
-// 가 들어갈 수 있도록 코드를 바꿔야 될 듯 하다.
-// main함수에 if를 걸면 안될 것 같은데.. 그러면 코드 자체에서 addRelatedTerm("name") 이런식으로 할 경우
-// termBox.value가 없는 상황이기때문에 if문 조건 충족이 되지 않아서 관련용어 추가가 안된다. ㅠ
+function validateTerm(name) {
+  if (termList.children.includes(name) === false) {
+    return;
+  }
+}
 // 그 다음, 중복이 안되게 하려면 어떻게 해야 할 지도 생각을 해봐야 한다. 각각의 if문에 넣어야 되는가?
