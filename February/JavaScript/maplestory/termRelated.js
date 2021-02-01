683cedf686bbca180395711d7bf6a73d1cb5c3e0
sangminpark <qpwpep5429@gmail.com>
2021-02-01 13:14:47 +0900

    February

---
+++ February/JavaScript/maplestory/termRelated.js
@@ -0,0 +1,57 @@
+const termSubmit = document.getElementById("btn_add_term");
+const termList = document.getElementById("list_terms");
+const termBox = document.getElementById("text_term");
+const termLi = document.querySelectorAll("ul li");
+
+termBox.addEventListener("keyup", removeSpecial);
+termSubmit.addEventListener("click", removeSpecial);
+function removeSpecial(e) {
+  e.target.value = e.target.value.replace(/[^ㄱ-힣a-zA-Z0-9+#]/gi, "");
+}
+
+termSubmit.addEventListener("click", () => {
+  addRelatedTerm(termBox.value);
+}, false);
+
+termBox.addEventListener("keyup", (e) => {
+  const keyCode = e.keyCode;
+  if (e.keyCode == 188 || e.keyCode == 32 || e.keyCode == 13) {
+    addRelatedTerm(termBox.value);
+  }
+});
+
+function addRelatedTerm(name) {
+  if (!name) {
+    return;
+  }
+  const trimmedTerm = name.trim();
+  const liElem = document.createElement("li");
+  liElem.innerText = trimmedTerm;
+
+  const removeBtn = document.createElement("button");
+  const item = document.querySelectorAll("#list_terms > li");
+  termList.appendChild(liElem);
+  liElem.appendChild(removeBtn);
+  removeBtn.innerHTML = "X";
+  for (let i = 0; i < item.length; i++) {
+    if (termList.children[i].innerText === name) {
+      return;
+    } else {
+      alert("중복입니다.");
+      termBox.value = "";
+    }
+    termBox.value = "";
+    removeBtn.addEventListener("click", () => {
+      removeBtn.parentNode.removeChild(removeBtn);
+      liElem.parentNode.removeChild(liElem);
+    });
+  }
+
+  function validateTerm(name) {
+    if (termList.children.includes(name) === false) {
+      return;
+    }
+  }
+}
+
+  // 그 다음, 중복이 안되게 하려면 어떻게 해야 할 지도 생각을 해봐야 한다. 각각의 if문에 넣어야 되는가?
---
+++ February/TypeScript/example.js
@@ -0,0 +1,11 @@
+function unrelated() {
+    console.log("unrelated");
+}
+
+function unrelated2() {
+    console.log("unrelated");
+}
+
+let cat;
+let chicken;
+let cheeseCake;
