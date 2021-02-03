072be4d57a7dbf6438c9130a4768f4d15361425e
sangminpark <qpwpep5429@gmail.com>
2021-02-03 12:09:30 +0900

    finish refactoring

--- February/JavaScript/maplestory/termRelated.js
+++ February/JavaScript/maplestory/termRelated.js
@@ -20,38 +20,59 @@ termBox.addEventListener("keyup", (e) => {
   }
 });
 
+// function addRelatedTerm(name) {
+//   if (!name) {
+//     return;
+//   }
+//   const trimmedTerm = name.trim();
+//   const liElem = document.createElement("li");
+//   liElem.innerText = trimmedTerm;
+
+//   const removeBtn = document.createElement("button");
+//   const item = document.querySelectorAll("#list_terms > li");
+//   for (let i = 0; i < item.length; i++) {
+//     if (JSON.stringify(termList.children[i].firstChild) !== name) {
+//       termList.appendChild(liElem);
+//       liElem.appendChild(removeBtn);
+//       removeBtn.innerHTML = "X";
+//       removeBtn.addEventListener("click", () => {
+//         removeBtn.parentNode.removeChild(removeBtn);
+//         liElem.parentNode.removeChild(liElem);
+//       });
+//     } else {
+//       alert("중복입니다.");
+//       termBox.value = "";
+//       return false;
+//     }
+//   }
+//   termBox.value = '';
+// }
 function addRelatedTerm(name) {
   if (!name) {
     return;
   }
   const trimmedTerm = name.trim();
+
   const liElem = document.createElement("li");
   liElem.innerText = trimmedTerm;
 
   const removeBtn = document.createElement("button");
-  const item = document.querySelectorAll("#list_terms > li");
-  termList.appendChild(liElem);
-  liElem.appendChild(removeBtn);
-  removeBtn.innerHTML = "X";
-  for (let i = 0; i < item.length; i++) {
-    if (termList.children[i].innerText === name) {
-      return;
-    } else {
-      alert("중복입니다.");
-      termBox.value = "";
-    }
-    termBox.value = "";
-    removeBtn.addEventListener("click", () => {
-      removeBtn.parentNode.removeChild(removeBtn);
-      liElem.parentNode.removeChild(liElem);
-    });
-  }
+  removeBtn.innerText = 'X';
+  removeBtn.addEventListener('click', () => {
+    removeBtn.parentNode.removeChild(removeBtn);
+    liElem.parentNode.removeChild(liElem);
+  })
 
-  function validateTerm(name) {
-    if (termList.children.includes(name) === false) {
+  const items = document.querySelectorAll("#list_terms > li");
+  for (let i = 0; i < items.length; i++) {
+    if (items[i].innerHTML.split('<')[0] === name) {
+      alert('중복 단어');
+      termBox.value = '';
       return;
     }
-  }
+  };
+  termList.appendChild(liElem).appendChild(removeBtn);
+  termBox.value = '';
+  return;
 }
-
-  // 그 다음, 중복이 안되게 하려면 어떻게 해야 할 지도 생각을 해봐야 한다. 각각의 if문에 넣어야 되는가?
+// 그 다음, 중복이 안되게 하려면 어떻게 해야 할 지도 생각을 해봐야 한다. 각각의 if문에 넣어야 되는가?
