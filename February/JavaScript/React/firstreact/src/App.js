57d7459841d60ac5af377455d900efeb19021263
박상민 <iamsangminpark@gmail.com>
2021-02-20 20:06:20 +0900

    ref

--- February/JavaScript/React/firstreact/src/App.js
+++ February/JavaScript/React/firstreact/src/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 // import Counter from './Counter';
 // import Say from './Say';
-import EventPractice from './EventPractice';
+// import EventPractice from './EventPractice';
 import './App.css';
+// import ValidationSample from './ValidationSample';
+import ScrollBox from './ScrollBox';
 
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
+        {/* <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button> */}
+      </div>
+    );
+  }
+}
 // // 함수형 컴포넌트
 // function App() {
 //   const name = 'React';
@@ -28,9 +42,9 @@ import './App.css';
 //   return <Counter />;
 // };
 
-const App = () => {
-  return <EventPractice />;
-};
+// const App = () => {
+//   return <EventPractice />;
+// };
 
 // App이라는 컴포넌트를 만들어 준다. function 키워드를 사용하여 컴포넌트를 만들었는데, 이러한 컴포넌트를 함수형 컴포넌트라고 부른다.
 // 프로젝트에서 컴포넌트를 렌더링하면(렌더링: '보여준다') 함수에서 반환하고 있는 내용을 나타낸다.
