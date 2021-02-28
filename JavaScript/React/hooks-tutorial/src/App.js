fa075af2a569a1d3509b234440cadddcaa348f2a
박상민 <iamsangminpark@gmail.com>
2021-02-28 17:36:48 +0900

    use Hooks

--- JavaScript/React/hooks-tutorial/src/App.js
+++ JavaScript/React/hooks-tutorial/src/App.js
@@ -1,22 +1,28 @@
-import React, { useState } from "react";
+import React from "react";
 // import Counter from "./Counter";
-import Info from "./Info";
+// import Info from "./Info";
+import Average from "./Average";
+
+// Info 렌더링
+// const App = () => {
+//   const [visible, setVisible] = useState(false);
+//   return (
+//     <div>
+//       <button
+//         onClick={() => {
+//           setVisible(!visible);
+//         }}
+//       >
+//         {visible ? "숨기기" : "보이기"}
+//       </button>
+//       <hr />
+//       {visible && <Info />}
+//     </div>
+//   );
+// };
 
 const App = () => {
-  const [visible, setVisible] = useState(false);
-  return (
-    <div>
-      <button
-        onClick={() => {
-          setVisible(!visible);
-        }}
-      >
-        {visible ? "숨기기" : "보이기"}
-      </button>
-      <hr />
-      {visible && <Info />}
-    </div>
-  );
+  return <Average />;
 };
 
 export default App;
--- JavaScript/React/hooks-tutorial/src/Info.js
+++ JavaScript/React/hooks-tutorial/src/Info.js
@@ -1,31 +1,76 @@
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
+
+// const Info = () => {
+//   const [name, setName] = useState("");
+//   const [nickname, setNickname] = useState("");
+
+//   useEffect(() => {
+//     console.log("effect");
+//     console.log(name);
+//     return () => {
+//       console.log("cleanup");
+//       console.log(name);
+//     };
+//   }, [name]);
+
+//   const onChangeName = (e) => {
+//     setName(e.target.value);
+//   };
+
+//   const onChangeNickname = (e) => {
+//     setNickname(e.target.value);
+//   };
+
+//   return (
+//     <div>
+//       <div>
+//         <input value={name} onChange={onChangeName} />
+//         <input value={nickname} onChange={onChangeNickname} />
+//       </div>
+//       <div>
+//         <div>
+//           <b>이름:</b> {name}
+//         </div>
+//         <div>
+//           <b>닉네임:</b> {nickname}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Info;
+
+import React, { useReducer } from "react";
+import useInputs from "./useInputs";
+
+// function reducer(state, action) {
+//   return {
+//     ...state,
+//     [action.name]: action.value,
+//   };
+// }
 
 const Info = () => {
-  const [name, setName] = useState("");
-  const [nickname, setNickname] = useState("");
+  // const [state, dispatch] = useReducer(reducer, {
+  //   name: "",
+  //   nickname: "",
+  // });
+  // const { name, nickname } = state;
+  // const onChange = (e) => {
+  //   dispatch(e.target);
+  // };
 
-  useEffect(() => {
-    console.log("effect");
-    console.log(name);
-    return () => {
-      console.log("cleanup");
-      console.log(name);
-    };
-  }, [name]);
-
-  const onChangeName = (e) => {
-    setName(e.target.value);
-  };
-
-  const onChangeNickname = (e) => {
-    setNickname(e.target.value);
-  };
-
+  const [state, onChange] = useInputs({
+    name: "",
+    nickname: "",
+  });
+  const { name, nickname } = state;
   return (
     <div>
       <div>
-        <input value={name} onChange={onChangeName} />
-        <input value={nickname} onChange={onChangeNickname} />
+        <input name="name" value={name} onChange={onChange} />
+        <input name="nickname" value={nickname} onChange={onChange} />
       </div>
       <div>
         <div>
