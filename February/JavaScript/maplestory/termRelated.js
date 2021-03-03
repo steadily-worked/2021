8042996f9dcca1bba7416988dbc8a61eba72c6d4
박상민 <iamsangminpark@gmail.com>
2021-03-03 13:33:52 +0900

    finish

--- February/JavaScript/maplestory/termRelated.js
+++ February/JavaScript/maplestory/termRelated.js
@@ -1,10 +1,16 @@
+var simplemde = new SimpleMDE({ element: document.getElementById("MyID") });
+
 const termSubmit = document.getElementById("btn_add_term");
 const termList = document.getElementById("list_terms");
 const termBox = document.getElementById("text_term");
 
-termSubmit.addEventListener("click", () => {
-  addRelatedTerm(termBox.value);
-}, false);
+termSubmit.addEventListener(
+  "click",
+  () => {
+    addRelatedTerm(termBox.value);
+  },
+  false
+);
 
 termBox.addEventListener("keyup", (e) => {
   const keyCode = e.keyCode;
@@ -14,7 +20,6 @@ termBox.addEventListener("keyup", (e) => {
 });
 
 function addRelatedTerm(name) {
-  const special = /[^ㄱ-힣a-zA-Z0-9+#]/gi;
   const newTerm = name.replace(/[^ㄱ-힣a-zA-Z0-9+#]/gi, "");
   if (!newTerm) {
     return;
@@ -26,23 +31,22 @@ function addRelatedTerm(name) {
   liElem.innerText = trimmedTerm;
 
   const removeBtn = document.createElement("button");
-  removeBtn.innerText = 'X';
-  removeBtn.addEventListener('click', () => {
+  removeBtn.innerText = "X";
+  removeBtn.addEventListener("click", () => {
     removeBtn.parentNode.removeChild(removeBtn);
     liElem.parentNode.removeChild(liElem);
-  })
+  });
 
   const items = document.querySelectorAll("#list_terms > li");
   for (let i = 0; i < items.length; i++) {
-    if (items[i].innerHTML.split('<')[0] === newTerm) {
-      alert('중복 단어');
-      termBox.value = '';
+    if (items[i].innerHTML.split("<")[0] === newTerm) {
+      alert("중복 단어");
+      termBox.value = "";
       return;
     }
-  };
+  }
   termList.appendChild(liElem).appendChild(removeBtn);
-  termBox.value = '';
+  termBox.value = "";
   return;
 }
 addRelatedTerm("**(*(*(*");
-// 그 다음, 중복이 안되게 하려면 어떻게 해야 할 지도 생각을 해봐야 한다. 각각의 if문에 넣어야 되는가?
