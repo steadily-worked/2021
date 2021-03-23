dc718d817d3f8b210df486a41e14b92cde54e54a
박상민 <iamsangminpark@gmail.com>
2021-03-23 22:17:42 +0900

    React ~Component Styling

--- JavaScript/React/styling-react/src/App.js
+++ JavaScript/React/styling-react/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
-import SassComponent from "./SassComponent";
+// import CSSModule from "./CSSModule";
+import StyledComponent from "./StyledComponent";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <SassComponent />
+        <StyledComponent />
       </div>
     );
   }
